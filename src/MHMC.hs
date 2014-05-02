e5d2f1da37827fe7d69f5762543860f12aa3915a
killmous <wcaldwell.email@gmail.com>
2014-05-02 15:57:42 -0500

    Monads are cool :D  Changes to be committed: 	modified:   src/MHMC.hs

--- src/MHMC.hs
+++ src/MHMC.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module MHMC
 (
     mhmc
@@ -9,6 +11,7 @@ import Graphics.Vty.Prelude
 import qualified Network.MPD as MPD
 
 import Data.Default
+import Data.Either
 
 data Screen = Help
             | Playlist
@@ -36,20 +39,21 @@ instance Show Screen where
 -- | command to run the program
 mhmc :: IO ()
 mhmc = do
+    status <- MPD.withMPD MPD.status
     vty <- mkVty def
     (width, height) <- displayBounds $ outputIface vty
-    update vty $ display (width, height) Help
+    update vty $ display (width, height) Help status
     e <- nextEvent vty
     shutdown vty
 
-display :: (Int, Int) -> Screen -> Picture
-display (width, height) Help =
-    header width Help
-display vty _ = emptyPicture
+display :: (Int, Int) -> Screen -> MPD.Response MPD.Status -> Picture
+display (width, height) screen status =
+    header width screen status
+display _ _ _ = emptyPicture
 
-header :: Int -> Screen -> Picture
-header width screen =
+header :: Int -> Screen -> MPD.Response MPD.Status -> Picture
+header width screen status =
     let title = string (def `withForeColor` brightBlack) $ show screen
-        volume = string (def `withForeColor` blue) "Volume: 79%"
+        volume = string (def `withForeColor` blue) ("Volume: " ++ (either (\_ -> "0") (show . MPD.stVolume) status) ++ "%")
     in addToTop (picForImage title) $ displayRight volume
     where displayRight image = translateX (width - imageWidth image) image
