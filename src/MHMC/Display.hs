f56d3b44f6510dba934742051c649137199c7a48
killmous <wcaldwell.email@gmail.com>
2014-05-04 15:11:15 -0500

    Added help documentation and gave up being able to tab and shift tab between tabs.  Changes to be committed: 	modified:   mhmc.cabal 	modified:   src/MHMC/Display.hs

--- src/MHMC/Display.hs
+++ src/MHMC/Display.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE QuasiQuotes #-}
 module MHMC.Display
 (
     Screen(..),
@@ -9,6 +10,7 @@ import qualified Network.MPD as MPD
 import Graphics.Vty
 
 import Data.Default
+import Data.String.QQ
 
 data Screen = Help
             | Playlist
@@ -20,7 +22,7 @@ data Screen = Help
             | Outputs
             | Visualizer
             | Clock
-            deriving (Enum)
+            deriving (Eq, Enum)
 
 instance Show Screen where
     show Help = "Help"
@@ -50,7 +52,31 @@ header width screen status =
     where displayRight image = translateX (width - imageWidth image) image
 
 contents :: (Int, Int) -> Screen -> MPD.Response MPD.Status -> Image
-contents (width, height) Help status = cropBottom (height - 4) $ pad 0 0 0 height $ string (def `withForeColor` brightBlack) "HELP SCREEN!"
+contents (width, height) Help status = cropBottom (height - 4)
+    $ pad 0 0 0 height
+    $ foldl1 (<->)
+    $ map (string (def `withForeColor` brightBlack))
+    $ lines helpinfo
+    where helpinfo = [s|
+
+    Keys - Movement
+--------------------------------
+        Up          : Move Cursor up
+        Down        : Move Cursor down
+
+        1           : Help screen
+        2           : Playlist screen
+        3           : Browse screen
+        4           : Search engine
+        5           : Media library
+        6           : Playlist editor
+        7           : Tag editor
+        8           : Outputs
+        9           : Music visualizer
+        0           : Clock screen
+
+        @           : MPD server info
+|]
 contents (width, height) _ _ = cropBottom (height - 4) $ pad 0 0 0 height emptyImage
 
 footer :: Int -> Screen -> MPD.Response MPD.Status -> Image
